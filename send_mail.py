5ae4571f777c55ede37c428bbe3a010baacefbe7
abhishek7071 <38905469+abhishek7071@users.noreply.github.com>
2020-06-22 17:50:39 +0530

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -6,12 +6,12 @@ def send_mail():
                server.ehlo()
                server.login('aman765180@gmail.com', 'Neesu@123')
                subject = "Price of " +str(product_name) +" has fallen down below Rs. " + str(desired_price)
-        body = "Hey Rahul! \n The price of Boat Headphone on AMAZON has fallen down below Rs." + str(
-            desired_price) + ".\n So, hurry up & check the amazon link right now : " + url
-        msg = f"Subject: {subject} \n\n {body} "
-        server.sendmail(
+               body = "Hey Rahul! \n The price of Boat Headphone on AMAZON has fallen down"  + ".\n So, hurry up & check the amazon link right now : " + url
+               msg = f"Subject: {subject} \n\n {body} "
+               server.sendmail(
             'sender_email',
             'Abhishek7071631646@gmail.com', msg
         )
-        
-        server.quit()
+               server.quit()
+
+

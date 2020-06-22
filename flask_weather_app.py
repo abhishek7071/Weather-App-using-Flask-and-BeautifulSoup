207cbb301b15e865aff12c4272e1d5c7664443dd
abhishek7071 <38905469+abhishek7071@users.noreply.github.com>
2020-06-22 17:54:15 +0530

    Update flask_weather_app.py

--- flask_weather_app.py
+++ flask_weather_app.py
@@ -33,22 +33,7 @@ def home():
         price = ''.join(price_ar)
     
         price = int(price)
-        def send_mail():
-               server = smtplib.SMTP('smtp.gmail.com', 587)
-               server.ehlo()
-               server.starttls()
-               server.ehlo()
-               server.login('aman765180@gmail.com', 'Neesu@123')
-               subject = "Price of " +str(product_name) +" has fallen down below Rs. " + str(desired_price)
-        body = "Hey Rahul! \n The price of Boat Headphone on AMAZON has fallen down below Rs." + str(
-            desired_price) + ".\n So, hurry up & check the amazon link right now : " + url
-        msg = f"Subject: {subject} \n\n {body} "
-        server.sendmail(
-            '',
-            'Abhishek7071631646@gmail.com', msg
-        )
         
-        server.quit()
         
         if desired_price >= price:
               send_mail()

b959e1bf808059066efc1472c3ce85fdecc63e54
abhishek7071 <38905469+abhishek7071@users.noreply.github.com>
2020-06-23 06:20:55 +0530

    Update flask_weather_app.py

--- flask_weather_app.py
+++ flask_weather_app.py
@@ -1,7 +1,7 @@
 from flask import Flask,request,render_template
 import requests
 from bs4 import BeautifulSoup
-
+from __main__ import *
 app = Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
